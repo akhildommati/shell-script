c44a46f86ad5ab0e3df160b15cd9acf910e83e9c
akhildommati <akhilprince567@gmail.com>
2025-02-08 14:08:26 -0500

    conversation added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo"Rahim :: Hi Robert"
-echo"Robert:: Hello Rahim"
-echo"Rahim :: How are you?"
-echo"Robert:: I am good, How about you?"
+echo "Rahim :: Hi Robert"
+echo "Robert:: Hello Rahim"
+echo "Rahim :: How are you?"
+echo "Robert:: I am good, How about you?"
