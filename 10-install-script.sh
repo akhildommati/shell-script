a3e8f4305d4bb7aa0bbb5045bea81321f716be12
akhildommati <akhilprince567@gmail.com>
2025-02-09 13:47:03 -0500

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,7 +5,8 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "ERROR ::You need to have root privileges to run this script"
+exit 1 # other than zero
 fi
-dnf install mysql -y
+dnf install mysqll -y
 
 dnf install git -y
