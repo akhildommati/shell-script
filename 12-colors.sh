f9d8b6228cfcedcd7943befcb0e3fd614941112f
akhildommati <akhilprince567@gmail.com>
2025-02-09 15:31:36 -0500

    shell script

---
+++ 12-colors.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+echo -e "$2...... $R FAILURE "
+exit 1
+else 
+echo  -e "$2...... $G SUCCESS"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then
+echo "ERROR ::You need to have root privileges to run this script"
+exit 1 # other than zero
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ] # not installed
+then
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
+else
+echo -e "MySQL is already.......$Y installed"
+fi
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+dnf install git -y
+VALIDATE $? "Installing GIT"
+else    
+echo -e "GIT is already.......$Y installed"
+fi
+
