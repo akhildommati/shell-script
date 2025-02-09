eae31a442e8e5c21377e89423ca920c3e1b4308a
akhildommati <akhilprince567@gmail.com>
2025-02-09 14:07:42 -0500

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -7,8 +7,12 @@ then
 echo "ERROR ::You need to have root privileges to run this script"
 exit 1 # other than zero
 fi
-dnf install mysql -y
 
+dnf list installed mysql
+
+if [ $? -ne 0 ] # not installed
+then
+dnf install mysql -y
 if [ $? -ne 0 ]
 then
 echo "ERROR :: MySQL installation failed"
@@ -16,9 +20,21 @@ exit 1
 else 
 echo "MySQL installation successful"
 fi
+else
+echo "MySQL is already installed"
+fi
+# if [ $? -ne 0 ]
+# then
+# echo "ERROR :: MySQL installation failed"
+# exit 1
+# else 
+# echo "MySQL installation successful"
+# fi
 
+dnf list installed git
+if [ $? -ne 0 ]
+then
 dnf install git -y
-
 if [ $? -ne 0 ]
 then
 echo "ERROR :: GIT installation failed"
@@ -26,3 +42,15 @@ exit 1
 else 
 echo "GIT installation successful"
 fi
+else    
+echo "GIT is already installed"
+fi
+
+
+# if [ $? -ne 0 ]
+# then
+# echo "ERROR :: GIT installation failed"
+# exit 1
+# else 
+# echo "GIT installation successful"
+# fi
