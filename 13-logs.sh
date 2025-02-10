c41610c022eae046ead8ccf8ad66079316c15646
akhildommati <akhilprince567@gmail.com>
2025-02-10 08:52:12 -0500

    script added

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,10 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE="$(echo $0 | cut -d "." -f1)"
+TIMESTAMP=$(date '+%d%m%Y-%H%M%S')
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -15,27 +19,28 @@ echo  -e "$2...... $G SUCCESS"
 fi
 }
 
+echo "Script started executing at : $TIMESTAMP" &>>$LOG_FILE
 if [ $USERID -ne 0 ]
 then
 echo "ERROR ::You need to have root privileges to run this script"
 exit 1 # other than zero
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE
 
 if [ $? -ne 0 ] # not installed
 then
-dnf install mysql -y
+dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing Mysql"
 else
 echo -e "MySQL is already.......$Y installed"
 fi
 
 
-dnf list installed git
+dnf list installed git &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-dnf install git -y
+dnf install git -y &>>$LOG_FILE
 VALIDATE $? "Installing GIT"
 else    
 echo -e "GIT is already.......$Y installed"
