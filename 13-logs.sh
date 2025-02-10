0be36b2bb38d541492e409b2834372cd2834d7e5
akhildommati <akhilprince567@gmail.com>
2025-02-10 09:01:41 -0500

    script added

--- 13-logs.sh
+++ 13-logs.sh
@@ -5,8 +5,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE="$(echo $0 | cut -d "." -f1)"
-TIMESTAMP=$(date '+%d-%m-%Y-%H-%M-%S')
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
